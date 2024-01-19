dc8a352d9e58749e58c3f279674a75f5c597bc23
zee-ham-su <hsufiian@yahoo.com>
2024-01-19 16:08:43 +0000

    routing

--- First_Project/myapp/views.py
+++ First_Project/myapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'myapp/index.html')
+    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
