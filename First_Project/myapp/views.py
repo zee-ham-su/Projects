5e90b6e828eda7ebbc4c59dd2126f484cf8ee6b7
zee-ham-su <hsufiian@yahoo.com>
2024-01-20 11:57:58 +0000

    views  and urls

--- First_Project/myapp/views.py
+++ First_Project/myapp/views.py
@@ -4,9 +4,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    context = {
-        'name': 'John',
-        'age': 23,
-        'color': 'sky blue',
-    }
-    return  render(request, 'index.html', context)
+    return  render(request, 'index.html')
+
+def counter(request):
+    words = request.GET['text']
+    wordlist = words.split()
+    worddictionary = {}
+    for word in wordlist:
+        if word in worddictionary:
+            #Increase
+            worddictionary[word] += 1
+        else:
+            #add to the dictionary
+            worddictionary[word] = 1
+    return render(request, 'counter.html', {'words':words, 'count':len(wordlist), 'worddictionary':worddictionary.items()})
