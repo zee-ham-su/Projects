348d9d61af4beb64152d51f5dd5e788e5dbda9a5
zee-ham-su <hsufiian@yahoo.com>
2024-01-21 19:08:51 +0000

    static file

--- First_Project/myapp/views.py
+++ First_Project/myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return  render(request, 'index.html')
+    return  render(request, 'index1.html')
 
 def counter(request):
     text = request.POST['text']
