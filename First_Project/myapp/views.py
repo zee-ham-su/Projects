1cf59e5e172c87e7b04fc6d862c84cd6aec8a831
zee-ham-su <hsufiian@yahoo.com>
2024-01-21 16:52:08 +0000

    word counter

--- First_Project/myapp/views.py
+++ First_Project/myapp/views.py
@@ -7,14 +7,6 @@ def index(request):
     return  render(request, 'index.html')
 
 def counter(request):
-    words = request.GET['text']
-    wordlist = words.split()
-    worddictionary = {}
-    for word in wordlist:
-        if word in worddictionary:
-            #Increase
-            worddictionary[word] += 1
-        else:
-            #add to the dictionary
-            worddictionary[word] = 1
-    return render(request, 'counter.html', {'words':words, 'count':len(wordlist), 'worddictionary':worddictionary.items()})
+    text = request.GET['text']
+    wordlist = len(text.split())
+    return render(request, 'counter.html', {'words': wordlist})
