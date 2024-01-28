e8faeb0caba023763365c5f08fe36d1bd0f03924
zee-ham-su <hsufiian@yahoo.com>
2024-01-28 17:43:06 +0000

    landing page

--- First_Project/myapp/views.py
+++ First_Project/myapp/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return  render(request, 'index1.html')
+    feature1 = Feature()
+    feature1.id = 1
+    feature1.name = 'Fast'
+    feature1.description = 'Our application is fast'
+    return  render(request, 'index.html', {'feature': feature1})
 
 def counter(request):
     text = request.POST['text']
